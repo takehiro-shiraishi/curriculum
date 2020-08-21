c0c92bbab419e952bf3d7de9fd646646d4652d6d
takeiroshiraishi <shirt8a1k0e@gmail.com>
2020-08-21 20:13:50 +0900

    2章HelloWorld課題提出
    index.phpの作成

---
+++ 2-1/index.php
@@ -0,0 +1,25 @@
+<?php
+// PHPの研修カリキュラム2-1
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-1
+*/
+?>
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+<?php
+echo "1.開発現場、検証環境、本番環境"."<br>";
+echo "開発環境は基本的には自分のパソコンでやることが多い複数人同士で、開発した機能を共有しあったりコードを結合して動作確認をしたりするための結合開発環境をリモートに用意することもある"."<br>";
+echo "検証環境は、開発し終わった、リリース予定の機能をテストするための環境です。開発環境と検証環境を分けておくことで、テスト中のシステムの状態を変更することなく、並行して別の開発を進めることができます"."<br>";
+echo "本番環境は実際にお客様が使用する環境のこと"."<br>";
+echo "2.基本設計書と詳細設計書"."</br>";
+echo "基本設計書はソフトウェアの動きを外から見た際にどういう動きをするのかを決めたもの"."<br>";
+echo "詳細設計は基本設計で決められた動きをどうやって実現するかを表したもの"."<br>";
+echo "3.Ajax"."<br>";
+echo "DOM,XML,JavaScriptを使い組み合わせたもの。Ajaxを使えば地図サイトを使う時などに読み込みの時間を減らすことができ直感的に快適に操作することができる"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
