a14f989818c42ca608a3ba3e3cc756708cc7ecef
takeiroshiraishi <shirt8a1k0e@gmail.com>
2020-08-31 23:17:22 +0900

    2-17課題提出
    2-17/index.php作成

--- 2-17/index.php
+++ 2-17/index.php
@@ -11,8 +11,23 @@ while($num <= 40) {
     $y ++;
 }
 echo "合計",$y,"回でゴールしました。";
+echo "<br>";
 ?>
 
 <?php
-
+date_default_timezone_set('Asia/Tokyo');
+$time = intval(date('H'));
+if (4 <= $time && $time <= 12) {
+    echo "今",$time,"時台です";
+    echo "<br>";
+    echo "おはようございます";
+} elseif (12 <= $time && $time <= 20 ) {
+    echo "今",$time,"時台です";
+    echo "<br>";
+    echo "こんにちは";
+} else {
+    echo "今",$time,"時台です";
+    echo "<br>";
+    echo "こんばんわ";
+}
 ?>
